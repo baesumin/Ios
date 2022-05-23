fc2120f4048d9d1841c08f3f376bd99652ce6863
SM <52240990+baesumin@users.noreply.github.com>
2022-05-23 11:08:10 +0900

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by SM on 2022/05/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by SM on 2022/05/23.
+// a
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
